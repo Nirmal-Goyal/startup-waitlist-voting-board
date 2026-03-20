3121542a0359236a2ee169e3ccfbd55697116376
Nirmal <nirmalgoyal0108@gmail.com>
2026-03-20 11:50:51 +0530

    feat: implement feature voting with anti-spam

---
+++ backend/src/controllers/featureController.js
@@ -0,0 +1,40 @@
+const {readDB, writeDB} = require("../utils/readWriteDB")
+
+exports.getFeatures = (req, res) => {
+    const db = readDB();
+    const sorted = db.features.sort((a, b) => b.votes - a.votes)
+    res.json(sorted)
+}
+
+exports.updateFeature = (req, res) => {
+    const {id} = req.params;
+    const userId = req.id;
+
+    const db = readDB()
+
+    const feature = db.features.find(f => f.id == id)
+
+    if(!feature){
+        return res.status(404).json({
+            message: "feature not found"
+        })
+    }
+
+    const alreadyVoted = db.votes.find(
+        v => v.userId === userId && v.featureId == id
+    )
+
+    if(alreadyVoted){
+        return res.status(400).json({
+            message: "you already voted"
+        })
+    }
+
+    feature.votes += 1
+
+    db.votes.push({userId, featureId: id})
+
+    writeDB(db)
+
+    res.json(feature)
+}
---
+++ backend/src/routes/featureRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+
+const {
+    getFeatures,
+    upvoteFeature
+} = require("../controllers/featureController")
+
+router.get("/", getFeatures)
+router.post("/:id/upvote", upvoteFeature)
+
+module.exports = router
