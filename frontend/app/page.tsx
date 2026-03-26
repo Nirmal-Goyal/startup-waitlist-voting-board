c79998f4bb15d340a025ce960c5ee291c20278e9
Ronak Jain <reachronakofficial@gmail.com>
2026-03-26 12:30:41 +0530

    Add: Frontend nextJs setup is added..

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+function Home() {
+  return (
+    <div className="flex flex-col text-white flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Hello
+    </div>
+  );
+}
+export default Home;
